c6225d06f292d184202ac3e55fed61499324fd5e
Faryan Rezagholi <faryan.rezagholi@siedle.de>
2021-09-22 08:56:43 +0200

    updated assembly info

--- ConvertOldOfficeFiles/Properties/AssemblyInfo.cs
+++ ConvertOldOfficeFiles/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConvertOldOfficeFiles")]
-[assembly: AssemblyDescription("Convert files with an old Excel and Word format (xls and doc) into new Office formats (xlsx, xlsm, docx, docm")]
+[assembly: AssemblyDescription("Convert files with an old Excel and Word format (xls and doc) into new Office formats (.xlsx, .xlsm, .docx and .docm)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConvertOldOfficeFiles")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
